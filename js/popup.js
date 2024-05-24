90520b684b99ce86c4e2ca0876e2389bd8cf1f63
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2024-05-24 11:55:22 +0530

    Changes made by bhuvana

--- js/popup.js
+++ js/popup.js
@@ -5,25 +5,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const popup = document.getElementById("popup");
   const closeBtn = document.getElementById("closeBtn");
   const body = document.body;
-  var count = 0;
+
   gsap.registerPlugin(ScrollTrigger);
-  // gsap code here!
+
   console.log("lolls");
   ScrollTrigger.create({
     trigger: "#popup-title",
     start: "top center",
     onEnter: () => {
-      
-      if(count == 0) {
-        body.classList.add("no-scroll"); // Disable scrolling
-        gsap.to(popup, {
-          display: "flex",
-          opacity: 1,
-          duration: 0.5,
-        });
-      }
-      count++;
-      
+      body.classList.add("no-scroll"); // Disable scrolling
+      gsap.to(popup, {
+        display: "flex",
+        opacity: 1,
+        duration: 0.5,
+      });
     },
   });
 
